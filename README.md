ecc69d2a84cc1cfaa3b45068ca3bcaa0b8042e74
João Silva <jgc3silva@gmail.com>
2021-11-27 01:20:52 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Core-CMSIS
+Build the DSP libraries from ARM Core CMSIS since they were removed from 5.8.0 onwards
+
+### How to build
+```
+cd build
+chmod +x build.sh
+./build.sh
+```
---
+++ build/build.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+RELEASE="5.8.0"
+git submodule init && git submodule update && cd ../CMSIS_5 && git checkout ${RELEASE} && cd -
+
+# Cortex-M7, Double Precision FPU
+cmake -DROOT=$(pwd)"/../CMSIS_5" \
+  -DCMAKE_PREFIX_PATH="/opt/gcc-arm-none-eabi" \
+  -DCMAKE_TOOLCHAIN_FILE="./gcc.cmake" \
+  -DARM_CPU="cortex-m7" \
+  -G "Unix Makefiles" ..
+make -j 6
+find bin_dsp/ -name "libCMSIS*.a" -exec arm-none-eabi-ar -x  {} \;
+arm-none-eabi-ar -qc libarm_cortexM7lfdp_math.a *.o
+rm *.o
+
+# Cortex-M4F, Single Precision FPU
+cmake -DROOT=$(pwd)"/../CMSIS_5" \
+  -DCMAKE_PREFIX_PATH="/opt/gcc-arm-none-eabi" \
+  -DCMAKE_TOOLCHAIN_FILE="./gcc.cmake" \
+  -DARM_CPU="cortex-m4" \
+  -G "Unix Makefiles" ..
+make -j 6
+find bin_dsp/ -name "libCMSIS*.a" -exec arm-none-eabi-ar -x  {} \;
+arm-none-eabi-ar -qc libarm_cortexM4lf_math.a *.o
+rm *.o
+
+# Get the official pack
+wget https://github.com/ARM-software/CMSIS_5/releases/download/${RELEASE}/ARM.CMSIS.${RELEASE}.pack && unzip ARM.CMSIS.${RELEASE}.pack -d ARM.CMSIS.${RELEASE}/ && rm ARM.CMSIS.${RELEASE}.pack
+
+# Create the Lib directory and move the built libraries there
+mkdir -p ARM.CMSIS.5.8.0/CMSIS/DSP/Lib/GCC/
+mv lib*.a ARM.CMSIS.5.8.0/CMSIS/DSP/Lib/GCC/
+
+echo "ARM CMSIS ready!"
+
